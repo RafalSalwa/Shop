1680b5c91ccf8cf12ecc2568aa91cb4f07d65c88
Rafal Salwa <rafal@salwa.com.pl>
2024-02-20 18:16:53 +0100

    created separate directory for tests and reports results

---
+++ reports/config/ecs.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    // this way you add a single rule
+    $ecsConfig->rules([
+        NoUnusedImportsFixer::class,
+        ArraySyntaxFixer::class,
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(
+        ArraySyntaxFixer::class,
+        [
+            'syntax' => 'short',
+        ]
+    );
+
+    $ecsConfig->sets([
+        SetList::SPACES,
+        SetList::ARRAY,
+        SetList::DOCBLOCK,
+        SetList::NAMESPACES,
+        SetList::COMMENTS,
+        SetList::PSR_12,
+        SetList::CLEAN_CODE,
+        SetList::COMMON,
+        SetList::SYMPLIFY,
+        SetList::DOCTRINE_ANNOTATIONS,
+        SetList::STRICT,
+        SetList::SYMPLIFY,
+    ]);
+};
