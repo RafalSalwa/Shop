b74b290e70bbc17cbad8a7f800577335e185aaf0
Rafal Salwa <rafal@salwa.com.pl>
2024-02-21 23:00:48 +0100

    cleaned up static files from public dir

--- public/mail/contact.php
+++ public/mail/contact.php
@@ -4,17 +4,18 @@ if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message'])
   exit();
 }
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$name = strip_tags(htmlspecialchars((string) $_POST['name']));
+$email = strip_tags(htmlspecialchars((string) $_POST['email']));
+$m_subject = strip_tags(htmlspecialchars((string) $_POST['subject']));
+$message = strip_tags(htmlspecialchars((string) $_POST['message']));
 
 $to = "info@example.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$subject = sprintf('%s:  %s', $m_subject, $name);
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: {$name}\n\n\nEmail: {$email}\n\nSubject: {$m_subject}\n\nMessage: {$message}";
+$header = 'From: ' . $email;
+$header .= 'Reply-To: ' . $email;	
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+if (!mail($to, $subject, $body, $header)) {
+    http_response_code(500);
+}
 ?>
