9a8598cce5123b007d2a1530f11232c8a81e2137
Rafal Salwa <rafal@salwa.com.pl>
2024-02-27 21:56:58 +0100

    Added fixes from twigcs, updated test suites for phpstan arkitekt extension

--- tests/Architecture/LayersTest.php
+++ tests/Architecture/LayersTest.php
@@ -1,14 +1,22 @@
 <?php
 
-use App\Command\AbstractSymfonyCommand;
+namespace App\Tests\Architecture;
 use PHPat\Selector\Selector;
 use PHPat\Test\PHPat;
 
-test('services does not depend on entities', function () {
-    return PHPat::rule()
-        ->classes(Selector::inNamespace('App\Command'))
-        ->shouldExtend()
-        ->classes(AbstractSymfonyCommand::class)
-        ->because('We want to be able to render html output in console outputs');
-
-});
+final class MyFirstTest
+{
+    public function test_domain_does_not_depend_on_other_layers(): Rule
+    {
+        return PHPat::rule()
+            ->classes(Selector::namespace('App\Domain'))
+            ->shouldNotDependOn()
+            ->classes(
+                Selector::namespace('App\Application'),
+                Selector::namespace('App\Infrastructure'),
+                Selector::classname(SuperForbiddenClass::class),
+                Selector::classname('/^SomeVendor\\\.*\\\ForbiddenSubfolder\\\.*/', true)
+            )
+            ->because('this will break our architecture, implement it another way! see /docs/howto.md');
+    }
+}
