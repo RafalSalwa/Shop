0a29adcbb9ac4131c95ab162f04dd7a499b4642d
Rafal Salwa <rafal@salwa.com.pl>
2024-03-22 15:03:23 +0100

    added common tests helpers to manipulate mocks for cart and cartItems

--- tests/Helpers/ProductHelperCartItemTrait.php
+++ tests/Helpers/ProductHelperCartItemTrait.php
@@ -4,20 +4,32 @@ namespace App\Tests\Helpers;
 
 use App\Entity\Product;
 use App\Entity\ProductCartItem;
+use ReflectionClass;
 
 trait ProductHelperCartItemTrait {
 
-    public function getHelperProductCartItem(): ProductCartItem
+    public function getHelperProductCartItem(int $id = 1): ProductCartItem
     {
         $productMock = $this->getMockBuilder(Product::class)
             ->disableOriginalConstructor()
             ->getMock();
-        $productMock->id = 1;
-        $productMock->name = 'Product 1';
-        $productMock->quantityPerUnit = 1;
-        $productMock->unitsInStock = 10;
-        $productMock->price = 100;
+        $product = new Product();
+        $this->setProtectedProperty($product, 'id', $id);
+        $this->setProtectedProperty($product, 'name', 'Product '. $id);
+        $this->setProtectedProperty($product, 'quantityPerUnit', 1);
+        $this->setProtectedProperty($product, 'name', 'unitsInStock '. 10);
+        $this->setProtectedProperty($product, 'price', 100);
 
-        return new ProductCartItem($productMock, 1);
+        $productCartItem = new ProductCartItem($product,1);
+        $this->setProtectedProperty($productCartItem, 'id', $id);
+        return $productCartItem;
+    }
+
+    private function setProtectedProperty($object, $property, $value)
+    {
+        $reflection = new ReflectionClass($object);
+        $reflection_property = $reflection->getProperty($property);
+        $reflection_property->setAccessible(true);
+        $reflection_property->setValue($object, $value);
     }
 }
